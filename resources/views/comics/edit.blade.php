daaee6a8c9c00e637056c3316bc62690298fac82
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-06-17 17:29:51 +0200

    Add content in edi page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -17,7 +17,7 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" name="title" id="title" class="form-control" placeholder="Comic title" aria-describedby="titleHelper" value="{{old('title')}}">
+            <input type="text" name="title" id="title" class="form-control" placeholder="Comic title" aria-describedby="titleHelper" value="{{$comic->title}}">
             <small id="titleHelper" class="text-muted">Type the comic name</small>
         </div>
         @error('title')
@@ -26,7 +26,7 @@
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input type="text" name="thumb" id="thumb" class="form-control" placeholder="Comic thumb" aria-describedby="thumbHelper" value="{{old('thumb')}}">
+            <input type="text" name="thumb" id="thumb" class="form-control" placeholder="Comic thumb" aria-describedby="thumbHelper" value="{{$comic->thumb}}">
             <small id="thumbHelper" class="text-muted">Type the thumb image</small>
         </div>
         @error('thumb')
@@ -35,7 +35,7 @@
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" name="price" id="price" class="form-control" placeholder="Comic price" aria-describedby="priceHelper" value="{{old('price')}}">
+            <input type="text" name="price" id="price" class="form-control" placeholder="Comic price" aria-describedby="priceHelper" value="{{$comic->price}}">
             <small id="priceHelper" class="text-muted">Price comic</small>
         </div>
         @error('price')
@@ -44,7 +44,7 @@
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" name="series" id="series" class="form-control" placeholder="Comic series" aria-describedby="seriesHelper" value="{{old('series')}}">
+            <input type="text" name="series" id="series" class="form-control" placeholder="Comic series" aria-describedby="seriesHelper" value="{{$comic->series}}">
             <small id="seriesHelper" class="text-muted">Type series</small>
         </div>
         @error('series')
@@ -53,7 +53,7 @@
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale date</label>
-            <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="Comic sale_date" aria-describedby="sale_dateHelper" value="{{old('sale_date')}}">
+            <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="Comic sale_date" aria-describedby="sale_dateHelper" value="{{$comic->sale_date}}">
             <small id="sale_dateHelper" class="text-muted">Sale date period</small>
         </div>
         @error('sale_date')
@@ -62,7 +62,7 @@
 
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
-            <input type="text" name="type" id="type" class="form-control" placeholder="Comic type" aria-describedby="typeHelper" value="{{old('type')}}">
+            <input type="text" name="type" id="type" class="form-control" placeholder="Comic type" aria-describedby="typeHelper" value="{{$comic->type}}">
             <small id="typeHelper" class="text-muted">Type comic</small>
         </div>
         @error('type')
@@ -72,7 +72,7 @@
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea class="form-control" name="description" id="description" rows="8">
-            {{old('description')}}
+            {{$comic->description}}
             </textarea>
             @error('description')
             <div class="alert alert-danger">{{ $message }}</div>
