347c8b65cfdecf9f56ba796dca3c95e68784eb33
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-06-17 18:34:20 +0200

    Add order by desc in comics.index page

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -16,7 +16,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        $comics = Comic::all();
+        $comics = Comic::orderByDesc('id')->get();
         return view('comics.index', compact('comics'));
     }
 
